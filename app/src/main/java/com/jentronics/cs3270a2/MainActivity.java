eb51d22de674d586cde1654a5b94fc012e8e6477
Ron Jensen <ron@jentronics.com>
2018-09-08 15:31:59 -0600

    Make button 2 work

--- app/src/main/java/com/jentronics/cs3270a2/MainActivity.java
+++ app/src/main/java/com/jentronics/cs3270a2/MainActivity.java
@@ -3,17 +3,39 @@ package com.jentronics.cs3270a2;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fm = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fm = getSupportFragmentManager();
         fm.beginTransaction()
                 .replace(R.id.fragAContainer, new FragmentA(), "ContainerA")
+                //.addToBackStack(null)
+                .commit();
+
+        Button btnLoad2 = (Button) findViewById(R.id.button_load_2);
+        btnLoad2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fm.beginTransaction()
+                        .replace(R.id.fragBContainer, new FragmentB(), "ContainerB")
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        fm.beginTransaction()
+                .replace(R.id.fragCContainer, new FragmentC(), "ContainerC")
+                //.addToBackStack(null)
+                .commit();
+        fm.beginTransaction()
+                .replace(R.id.fragDContainer, new FragmentD(), "ContainerD")
                 //.addToBackStack(null)
                 .commit();
     }
